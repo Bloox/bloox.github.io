7727618789160501569ecf4a41fc90d96ad16570
dramori <joachim.mogielka@gmail.com>
2024-06-24 13:32:28 +0200

    c

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 window.onload = () => {
     keywords = /`(.*?)`/g
+
+    specials = /\[(.*?)\](.*?)\[\/\1\]/g
+    specials_dict = {
+        'lang' : (this,lang)=>{
+            langs = {
+                'pl':"Polski :pl:",
+                "en":"English :en:"
+            }
+            this.innerHTML = langs[lang]
+
+        }
+    }
     document.body.innerHTML = document.body.innerHTML.replace(keywords,"<span class='keywords'>$1</span>")
+
+    document.body.innerHTML = document.body.innerHTML.replace(specials,"<span onload='specials_dict[\"$1\"](this,\"$2\")'>$2<span>")
 }
